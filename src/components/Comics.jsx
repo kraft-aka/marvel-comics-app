0d7dff42952411766b2229a39a900d00b84d9bc9
kubat <kubat.aiupov@gmail.com>
2024-05-03 12:13:35 +0200

    add basic style to btn, add format date func

--- src/components/Comics.jsx
+++ src/components/Comics.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import '../styles/Comics.scss'
     
 const Comics = ({ data }) => {
+  const formatDate = (d) => {
+    d = new Date();
+    return `${d.getDate()}-${d.getMonth()+1}-${d.getFullYear()}`
+  } 
   console.log(data)
   return (
     <div className='comics-container'>
-      { data.map(comics => ( <li>{comics?.description} sale date: {comics?.dates[0]?.date}</li> )) }
+      { data.map(comics => ( <div className='comics-card'>{comics?.description} sale date: {formatDate(comics?.dates[0]?.date)}</div> )) }
     </div>
   )
 }
